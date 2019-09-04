fc39bbef2e0153e582b025153b7ee467faee1d51
Jaime Ortiz <jxme88@gmail.com>
2019-09-04 14:51:48 -0700

    Starting over

--- src/App.js
+++ src/App.js
@@ -1,17 +1,1 @@
-import React from 'react';
-import WeatherCard from './components/WeatherCard';
-import { Header } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <Header>
-        Weather App
-      </Header>
-
-      <WeatherCard />
-    </div>
-  );
-}
-
-export default App;
