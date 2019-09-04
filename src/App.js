d750cc2bd625037dc7c3ec658f7773e7bce16c49
Jaime Ortiz <jxme88@gmail.com>
2019-09-04 16:45:34 -0700

    Added components and weather api

--- src/App.js
+++ src/App.js
@@ -1,1 +1,72 @@
 
+import React from 'react';
+import Title from './components/Title';
+import Form from './components/Form';
+import Weather from './components/Weather';
+
+// const API_KEY = 'cd8474bb3d13e42db8ff19a7ee5ee3df';
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+class App extends React.Component {
+
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  };
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=metric`);
+
+    const data = await api_call.json();
+
+    if (city && country) {
+      console.log(data);
+
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ''
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: 'Please enter these values homie.'
+      });
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <Title/>
+        <Form getWeather={this.getWeather}/>
+        <Weather 
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+/>
+      </div>
+    );
+  };
+};
+
+export default App;
